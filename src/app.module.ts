10985a5151247112f8cfe3b32fcc5143c13d828a
Eventlistener <chanfcsg334@gmail.com>
2024-10-09 13:39:44 +0700

    initilize project

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TodoModule } from './todos/todos.module';
+import { Todo } from './todos/todo.entity';
+import { join } from 'path';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_host'),
+        port: +configService.get('DB_port'),
+        username: configService.get('DB_username'),
+        password: configService.get('DB_password'),
+        database: configService.get('DB_database'),
+        entities: [join(process.cwd(), 'dist/**/*.entity.js')],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    TodoModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/todos/todos.module.ts
@@ -0,0 +1,8 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Todo } from "./todo.entity";
+
+@Module({
+    imports:[TypeOrmModule.forFeature([Todo])]
+})
+export class TodoModule {}
